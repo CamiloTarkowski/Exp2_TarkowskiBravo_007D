d5edc089b8d495988b58e3930d761531e241b5fb
CamiloTarkowski <camilo.tarkowski@gmail.com>
2021-05-16 21:44:04 -0400

    inicio nuevo, comentarios validados, eventos aplicados

--- js/validaciones.js
+++ js/validaciones.js
@@ -9,7 +9,9 @@ function valiRegistro()
                 confemail= document.getElementById('confemail').value;
                 pass = document.getElementById('pass').value;
                 confpass = document.getElementById('confpass').value; 
-                genero = document.getElementById('genero').value; 
+                genero = document.getElementById('genero').value;
+                re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+                terminos=document.getElementById('terminos').value; 
 
                 if(nom == null || nom.length==0 || /^\s+$/.test(nom))
                 {
@@ -46,18 +48,13 @@ function valiRegistro()
                     document.datos.fono.focus();
                     return false;
                 }
-                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email))
-                {
-                    alert("La dirección de email " + email + " es correcta.");
-                   } 
-                else {
-                    alert("La dirección de email es incorrecta.");
+                if(!re.exec(email)){
+                    alert('El email ingresado no es válido');
+                    document.datos.email.focus();
                     return false;
                 }
-                if(confemail == null || confemail.length==0 || /^\s+$/.test(confemail))
-                {
-                    alert('Debe confirmar su email');
-                    document.getElementById('confemail').value="";
+                if(!re.exec(confemail)){
+                    alert('Confirmación de email no válida');
                     document.datos.confemail.focus();
                     return false;
                 }
@@ -82,22 +79,84 @@ function valiRegistro()
                     document.datos.genero.focus();
                     return false;
                 }
-                if (confemail!=email)
+
+                if (document.datos.terms.checked == false)
+                {
+                alert('Debe aceptar los términos y condiciones para continuar');
+                return false;
+                }
+
+                if (confemail != email)
                 {
                     alert('Error. Confirmación de email incorrecta');
                     document.datos.confemail.focus();
                     return false;
 
                 }
-                if (pass!=confpass)
+                if (pass != confpass)
                 {
                     alert('Error. Las contraseñas deben ser las mismas');
                     document.datos.confpass.focus();
                     return false;
 
                 }
-               
+                alert('¡BIENVENIDO(A), '+nom+'! SU REGISTRO FUE EXITOSO.')
                 return true;      
 
                 
             }
+function valiComentario(){
+          nombre= document.getElementById('nombre').value;
+          rut= document.getElementById('rut').value;
+          fono= document.getElementById('fono').value;
+          email= document.getElementById('email').value;
+          tipocoment = document.getElementById('tipocomentario').value;
+          comentario=document.getElementById('comentario').value;
+          re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+                if(nombre == null || nombre.length==0 || /^\s+$/.test(nombre))
+                {
+                alert('Debe escribir su nombre completo');
+                    document.getElementById('nombre').value="";
+                    document.datos.nombre.focus();
+                    return false;
+                }
+                if(rut == null || rut.length==0 || /^\s+$/.test(rut))
+                {
+                    alert('Debe escribir su RUT');
+                    document.getElementById('rut').value="";
+                    document.datos.rut.focus();
+                    return false;
+                }
+                if(!(/^\d{9}$/.test(fono)) )
+                 {
+                    alert('Debe ingresar un teléfono válido');
+                    document.getElementById('fono').value="";
+                    document.datos.fono.focus();
+                    return false;
+                }
+                if(!re.exec(email)){
+                    alert('El email ingresado no es válido');
+                    document.datos.email.focus();
+                    return false;
+                }
+                
+                
+                if (tipocoment == null || tipocoment == 0)
+                {
+                    alert('Seleccione tipo de comentario');
+                    document.datos.genero.focus();
+                    return false;
+                }
+                if(comentario == null || comentario.length==0 || /^\s+$/.test(comentario))
+                {
+                alert('Debe escribir su comentario');
+                    document.getElementById('nombre').value="";
+                    document.datos.nom.focus();
+                    return false;
+                }
+
+                alert('Gracias por tu comentario. Tu opinión nos ayuda a mejorar')
+                return true;
+
+}
